c7e24b688abacdea9b02590917fd2be44e0b01da
zssdhl <zss200ok@163.com>
2016-08-05 20:05:25 +0800

    使用flask开发web部分

---
+++ router/controller.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+@author: zingzheng
+'''
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
